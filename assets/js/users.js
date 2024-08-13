4e4ea4b65bb39074bd6652d8a379fec1f943bb5c
ayda <aydaahadadii847@gmail.com>
2024-08-13 12:10:18 +0330

    part9

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+  
+  
+  
+    const addUserButton = document.getElementById('add-user-button')
+    addUserButton.addEventListener('click' , ()=>{
+      document.querySelector('.add-user-modal').classList.add('show')
+      document.querySelector('.add-user-modal-back').classList.remove('dis-none')
+    })
+  
+    document.querySelector('.add-user-modal-back').addEventListener('click', ()=>{
+      document.querySelector('.add-user-modal').classList.remove('show')
+      document.querySelector('.add-user-modal-back').classList.add('dis-none')
+})
+  
   });
+
+
+  
